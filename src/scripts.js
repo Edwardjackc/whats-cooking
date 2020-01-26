6039106f267a19b8ed6c4f8942cb5863b3ac3268
Edward Cheatham <edwardjackc@gmail.com>
2020-01-26 15:14:22 -0700

    Add recipes constructor

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,8 @@ let siteData = 'siteData'
 //local storage 
 localData = JSON.parse(localStorage.getItem('siteData'))
 localStorage.setItem('siteData', JSON.stringify(siteData))
+
+
 console.log(siteData)
 console.log(localStorage.getItem('siteData'))
 
