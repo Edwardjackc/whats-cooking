ce5a6c93cde94caefd9243acbdb8cec5c35217ac
Edward Cheatham <edwardjackc@gmail.com>
2020-02-13 13:54:38 -0700

    Add function framework

--- src/recipes.js
+++ src/recipes.js
@@ -1,11 +1,23 @@
 class Recipe {
-  constructor (){
+  constructor (recipe){
     this.id = recipe.id; 
     this.name = recipe.name;
     this.quantity = recipe.quantity; 
     this.amount = recipe.amount;
     this.unit = recipe.unit; 
   }
+}
+
+calculateCost(){
+
+}
+
+displayInstructions(){ 
+
+}
+
+displayIngredients(){ 
+
 }
 
 
--- src/user.js
+++ src/user.js
@@ -1,7 +1,27 @@
 class User{
-  constructor() {
+  constructor(user) {
     this.is = user.id; 
     this.name = user.name; 
     this.pantry = user.pantry; 
+
+  }
+  storeRecipes(type,recipe){
+  }
+
+
+  removeRecipe(type,recipe){
+
+  }
+
+  filterRecipes(type,tag) {
+
+  }
+  
+  searchRecipes(type,keyword){
+
+  }
+
+  loadRecipes(){
+
   }
 }
